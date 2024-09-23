492500fa0b8450f729126d6436d21f1992e2d574
juliaapolonio <apoloniojulia@gmail.com>
2024-09-23 16:25:03 -0300

    refact: Alter move_sign_genes¨

--- bin/move_sign_genes.sh
+++ bin/move_sign_genes.sh
@@ -2,19 +2,19 @@
 
 # Check if the correct number of arguments is provided
 if [ "$#" -ne 3 ]; then
-    echo "Usage: $0 <source_dir> <destination_dir> <gene_list>"
+    echo "Usage: $0 <gene_list>"
     exit 1
 fi
 
 # Assign command-line arguments to variables
-source_dir="$1"
-destination_dir="$2"
-gene_list="$3"
+gene_list="$1"
+
+destination_dir = ./filtered
 
 # Iterate over each gene name in the list
 while IFS= read -r gene_name; do
     # Search for files matching the gene name pattern in the source directory
-    matching_files=("$source_dir"/*"${gene_name}"_*)
+    matching_files=(./*"${gene_name}"_*)
     
     # Move matching files to the destination directory
     for file in "${matching_files[@]}"; do
